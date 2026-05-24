c578598f737bc8380d8186aba8168a7134642884
Mohammed Ammar <m.judo2005@gmail.com>
2026-05-24 20:06:39 +0300

    Add files via upload

---
+++ AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+<Aboutus1/>
+<Aboutus2/>
+<Aboutus3/>
+<Aboutus4/>
+<Aboutus5/>
+</template>
+
+<script setup>
+
+import Aboutus1 from '@/components/About/Aboutus1.vue';
+import Aboutus2 from '@/components/About/Aboutus2.vue';
+import Aboutus3 from '@/components/About/Aboutus3.vue';
+import Aboutus4 from '@/components/About/Aboutus4.vue';
+import Aboutus5 from '@/components/About/Aboutus5.vue';
+
+</script>
